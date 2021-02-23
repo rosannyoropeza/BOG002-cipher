d13c28a428d705a1b302095aa5461b2bf64789c5
Tatiroa994 <tatico994@gmail.com>
2021-02-23 12:00:39 -0500

    cambios dia 2

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,6 @@
 const cipher = {
-  // ...
-};
+
+}
 
 export default cipher;
+ 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+
+let mensaje = document.getElementById("mensaje");
+
+mensaje.addEventListener("keyup", function(){
+  mensaje.value = mensaje.value.toUpperCase(); 
+  console.log(mensaje.value);
+});
+
 import cipher from './cipher.js';
 
 console.log(cipher);
+
