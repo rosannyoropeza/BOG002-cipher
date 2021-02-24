2caf4a638916667805fe01bb6be98cc9f75669c6
Tatiroa994 <tatico994@gmail.com>
2021-02-24 11:49:13 -0500

    cambios dia  3

--- cipher.js
+++ cipher.js
@@ -1,6 +1,15 @@
 const cipher = {
 
+    //  decifrar 
+    decode: function (offsetGuardado, mensajeGuardado) {
+        console.log(offsetGuardado, mensajeGuardado);
+    },
+
+    //cifrar
+    encode: function (offsetGuardado, mensajeGuardado) {
+        console.log(offsetGuardado, mensajeGuardado);
+
+    }
 }
 
 export default cipher;
- 
--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-
-let mensaje = document.getElementById("mensaje");
-
-mensaje.addEventListener("keyup", function(){
-  mensaje.value = mensaje.value.toUpperCase(); 
-  console.log(mensaje.value);
-});
-
 import cipher from './cipher.js';
 
-console.log(cipher);
+let mensaje = document.getElementById("mensaje");
+let offset = document.getElementById("offset");
+let botonDecode = document.getElementById("button_decode");
+let botonEncode = document.getElementById("button_encode");
+let mensajeGuardado = " ";
+let offsetGuardado = 0;
+
+mensaje.addEventListener("keyup", function () {
+  mensaje.value = mensaje.value.toUpperCase();
+  mensajeGuardado = mensaje.value;
+  //console.log(mensajeGuardado);
+});
+
+botonEncode.addEventListener("click", function () {
+  offsetGuardado= offset.value;
+  cipher.encode(offsetGuardado, mensajeGuardado);
+
+});
+
+botonDecode.addEventListener("click", function () {
+  offsetGuardado= offset.value;
+  cipher.encode(offsetGuardado, mensajeGuardado);
+});
+
+
+console.log(cipher.decode);
 
