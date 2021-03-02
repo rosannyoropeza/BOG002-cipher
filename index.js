952108108c78eb7d3a91bdc9a33bb27226b2f34a
Tatiroa994 <tatico994@gmail.com>
2021-03-01 19:41:08 -0500

    modal

--- index.js
+++ index.js
@@ -4,29 +4,40 @@ let mensaje = document.getElementById("mensaje");
 let offset = document.getElementById("offset");
 let botonDecode = document.getElementById("button_decode");
 let botonEncode = document.getElementById("button_encode");
-let mensajeFinal = document.getElementById("mensajefinal");
 let mensajeGuardado = " ";
 let offsetGuardado;
-let titulomensaje = document.getElementById("titulomensaje");
+let modal_container = document.getElementById("modal_container");
+let cerrar = document.getElementById("cerrar");
+let modal = document.getElementById("modal");
+let enviarBoton = document.getElementById("enviar");
 
 /*mensaje.addEventListener("keyup", function () {
   mensaje.value = mensaje.value.toUpperCase();
   mensajeGuardado = mensaje.value;
   //console.log(mensajeGuardado);
 });*/
+cerrar.addEventListener("click", close);
+function close() {
+  modal_container.classList.remove("show");
+}
 
 botonEncode.addEventListener("click", function () {
   //boton cifrar
   mensajeGuardado = mensaje.value;
   offsetGuardado = parseInt(offset.value);
   if (!offsetGuardado) {
-    alert("INGRESAR CLAVE");
+    modal_container.classList.add("show");
+    modal.innerHTML = `<p> INGRESA CLAVE </P>`;
+    enviarBoton.style.display = "none";
   } else {
     let mensajeFinalGuardado = cipher.encode(offsetGuardado, mensajeGuardado);
-    titulomensaje.innerHTML = "TU MENSAJE CIFRADO ES:";
-    mensajeFinal.innerHTML = mensajeFinalGuardado;
+    modal_container.classList.add("show");
+    modal.innerHTML = `<h3> TU MENSAJE CIFRADO ES:</h3><br>
+    ${mensajeFinalGuardado}<br>`;
+    enviarBoton.style.display = "inline";
+
     //console.log(typeof offsetGuardado, mensajeGuardado); // validar el  tipo  de dato que offset
-    console.log(mensajeFinalGuardado);
+    //console.log(mensajeFinalGuardado);
   }
 });
 
@@ -35,12 +46,14 @@ botonDecode.addEventListener("click", function () {
   mensajeGuardado = mensaje.value;
   offsetGuardado = parseInt(offset.value);
   if (!offsetGuardado) {
-    alert("INGRESAR CLAVE");
+    modal_container.classList.add("show");
+    modal.innerHTML = `<p> INGRESA CLAVE </P>`;
   } else {
     let mensajeFinalGuardado = cipher.decode(offsetGuardado, mensajeGuardado);
-    console.log(mensajeFinalGuardado);
-    titulomensaje.innerHTML = "TU MENSAJE DESCIFRADO ES:";
-    mensajeFinal.innerHTML = mensajeFinalGuardado;
+    modal_container.classList.add("show");
+    modal.innerHTML = `<h3> TU MENSAJE DESCIFRADO ES:</h3><br>
+    ${mensajeFinalGuardado}<br>`;
+    enviarBoton.style.display = "inline";
     //console.log(cipher.decode);
   }
 });
